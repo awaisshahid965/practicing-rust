00ac7904f2fa36b3e06302001d400c7d866a01aa
Muhammad Awais <awaisshahid965@gmail.com>
2024-04-03 22:18:30 +0500

    created function to calculate power

---
+++ src/power.rs
@@ -0,0 +1,13 @@
+pub fn pow(base: i32, exponent: i32) -> f32 {
+    let mut result = 1.0;
+    let loop_range = if exponent < 0 { -exponent } else { exponent };
+    
+    for _ in 1..=loop_range {
+        result *= base as f32;
+    }
+    
+    if exponent < 0 {
+        return 1.0/result;
+    }
+    result
+}
