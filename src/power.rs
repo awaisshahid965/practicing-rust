064ce1c4f17c937598a607cd3e846acb1635daec
Muhammad Awais <awaisshahid965@gmail.com>
2024-04-05 22:49:28 +0500

    added types to function

--- src/power.rs
+++ src/power.rs
@@ -1,6 +1,6 @@
 pub fn pow(base: i32, exponent: i32) -> f32 {
-    let mut result = 1.0;
-    let loop_range = if exponent < 0 { -exponent } else { exponent };
+    let mut result: f32 = 1.0;
+    let loop_range: i32 = if exponent < 0 { -exponent } else { exponent };
     
     for _ in 1..=loop_range {
         result *= base as f32;
